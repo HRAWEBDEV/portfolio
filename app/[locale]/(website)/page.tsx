156da72452ef8bf850a680be33abe708cb05372c
hamidwise1 <hrawebdev@gmail.com>
2024-08-27 09:59:24 +0330

    update page

--- app/[locale]/(website)/page.tsx
+++ app/[locale]/(website)/page.tsx
@@ -1,11 +1,3 @@
-import Hero from './components/Hero';
-import News from './components/News';
-
 export default function Home() {
- return (
-  <div>
-   <Hero />
-   <News />
-  </div>
- );
+ return <div></div>;
 }
